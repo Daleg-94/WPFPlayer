2de66e14a51f9c2020ba443a0c945fc820dca14f
Daleg Daleg <daleg94@gmail.com>
2023-11-12 15:16:44 +0300

    Добавлен базовый класс модели представления

---
+++ Interface/IMediaService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFPlayer.Interface
+{
+    internal interface IMediaService
+    {
+        string OpenMediaFile();
+        string FilterString(string input);
+
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+using WPFPlayer.ViewModels.Base;
+using WPFPlayer.Controls;
+
+namespace WPFPlayer.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
